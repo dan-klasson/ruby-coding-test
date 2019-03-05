e7faa4bf88e82d5dfbce3b4e51a1e969752b7ce7
Dan <dan.g.klasson@gmail.com>
2019-03-06 01:47:47 +0700

    fixed entry with nil bug & added regression test

--- spec/requests/leaderboards_spec.rb
+++ spec/requests/leaderboards_spec.rb
@@ -2,9 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Leaderboards", type: :request do
   describe "GET /leaderboards" do
-    it "works! (now write some real specs)" do
+    it "displays leaderboard list" do
+      Leaderboard.create!({name: "Test Leaderboard"})
       get leaderboards_path
-      expect(response).to have_http_status(200)
+      expect(response.status).to be 200
+      expect(response.body).to include("Test Leaderboard")
+    end
+  end
+  describe "GET /leaderboards/:id" do
+    it "shows the leaderboard" do
+      leaderboard = Leaderboard.create!({name: "Test Leaderboard"})
+      get leaderboard_path leaderboard.id
+      expect(response.status).to be 200
+      expect(response.body).to include("Test Leaderboard")
+    end
+    it "handles entries with nil" do
+      leaderboard = Leaderboard.create!({name: "Test Leaderboard"})
+      LeaderboardEntry.create!(score: 10, leaderboard: leaderboard)
+      LeaderboardEntry.create!(score: nil, leaderboard: leaderboard)
+      get leaderboard_path leaderboard.id
+      expect(response.status).to be 200
     end
   end
 end
